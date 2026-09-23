36963f320eff1265c4d0f99e1fa8e7871d99781a
Androidest <stcoda@github.com>
2026-09-23 19:42:19 +0800

    Added chosen lesson content
    Signed-off-by: Androidest <stcoda@github.com>

--- app/ui/tab_lesson.py
+++ app/ui/tab_lesson.py
@@ -3,10 +3,10 @@ from app.utils.paths import PATH_LESSONS
 from app.utils.sheet import Sheet
 import json
 
-def get_lesson_meta(sheet: Sheet) -> dict:
+def get_lesson_meta(sheet: Sheet, progress_idx: int = 0) -> dict:
     # TODO
     return {
-        "test": "test", 
+        "progress_idx": progress_idx, 
     }
 
 def generate_lesson(sheet: Sheet):
@@ -18,7 +18,7 @@ def generate_lesson(sheet: Sheet):
 def load_lessons() -> tuple:
     lessons_meta: dict = {} # meta is for fast access to some breif info of each lesson without loading all the sheets
     lessons: dict = {} # contains the full info of each lesson including handlers of sheets and meta
-    cur_lesson: str = -1
+    cur_lesson: str = None
 
     if PATH_LESSONS.exists():
         # load meta info
@@ -40,6 +40,10 @@ def load_lessons() -> tuple:
                 lessons[lesson_name]["sheet"] = sheet
                 lessons[lesson_name]["meta"] = get_lesson_meta(sheet)
                 has_missing_meta = True
+
+            elif cur_lesson is not None and cur_lesson == lesson_name and lessons[lesson_name]["sheet"] is None:
+                # if current lesson is the same as the lesson name, load the sheet before entering the lesson
+                lessons[lesson_name]["sheet"] = Sheet(file_path) # same in on_choose_lesson
 
         if has_missing_meta:
             save_meta(lessons, cur_lesson)
@@ -63,7 +67,7 @@ def delete_lesson_sheet(lesson_name: str):
         path.unlink()
 
 def load_meta() -> tuple:
-    cur_lesson = -1
+    cur_lesson = None
     lessons_meta = {}
 
     meta_path = PATH_LESSONS/"meta.json"
@@ -87,6 +91,13 @@ def save_meta(lessons: dict, cur_lesson: str):
 
     with open(PATH_LESSONS/"meta.json", "w", encoding="utf-8") as f:
         json.dump(new_meta, f, ensure_ascii=False, indent=4)
+
+def update_meta(lessons: dict, cur_lesson: str, progress_idx: int):
+    new_lessons = lessons.copy()
+    new_lessons[cur_lesson]["meta"] = get_lesson_meta(new_lessons[cur_lesson]["sheet"], progress_idx)
+    save_meta(new_lessons, cur_lesson)
+
+    return new_lessons, cur_lesson
 
 def on_delete_lesson(old_lessons: dict, cur_lesson: str):
     new_lessons = old_lessons.copy()
@@ -124,12 +135,13 @@ def on_click_add_lesson():
 
 def on_choose_lesson(lesson_name: str, old_lessons: dict):
     print(f"Choose lesson: {lesson_name}")
-    new_cur_lesson = lesson_name
 
+    new_cur_lesson = lesson_name
     # check if the lesson exists
     if lesson_name not in old_lessons:
         raise ValueError(f"Lesson {lesson_name} does not exist")
 
+    new_lessons = old_lessons
     # update lesson sheet
     if old_lessons[lesson_name].get("sheet", None) is None:
         path = PATH_LESSONS / f"{lesson_name}.xlsx"
@@ -138,67 +150,85 @@ def on_choose_lesson(lesson_name: str, old_lessons: dict):
 
         new_lessons = old_lessons.copy()
         new_lessons[lesson_name]["sheet"] = Sheet(path)
-        return new_lessons, new_cur_lesson
 
-    return old_lessons, new_cur_lesson
+    save_meta(new_lessons, new_cur_lesson)
+    return new_lessons, new_cur_lesson
 
 def on_exit_lesson():
-    return -1 # cur_lesson is -1 when no lesson is selected, back to the lessons list
+    return None # cur_lesson is None when no lesson is selected, back to the lessons list
 
-def on_change_cur_llm(llm_configs: list[dict], cur_llm: int):
-    cur_llm_name = "-- Not Selected --"
-    if cur_llm != -1 and len(llm_configs) > cur_llm:
-        cur_llm_name = llm_configs[cur_llm]["alias"]
-    return cur_llm_name
-
-# TODO
 def render_tab_lesson(state_lessons: gr.State, state_cur_lesson: gr.State, state_llm_configs: gr.State, state_cur_llm: gr.State):
-    with gr.Tab("Lessons") as tab_lesson:
 
-        cur_ai = gr.Text("", label="Current AI")
-        state_cur_llm.change(
-            on_change_cur_llm, 
-            inputs=[state_llm_configs, state_cur_llm],
-            outputs=[cur_ai])
+    with gr.Tab("Lessons"):
 
-        with gr.Row():
-            # list of lessons
-            @gr.render(inputs=[state_lessons])
-            def render_items(lessons: dict):
-                
-                # TODO different sorting options
-                # sort lessons by name
-                sorted_lessons = sorted(lessons.items(), key=lambda item: item[1]["name"])
+        @gr.render(inputs=[state_lessons, state_cur_lesson, state_llm_configs, state_cur_llm])
+        def render_items(lessons: dict, cur_lesson: str, llm_configs: list[dict], cur_llm: int):   
 
-                # render all the lesson items
-                for lesson_name, lesson in sorted_lessons:
-                    # data
-                    meta = lesson.get("meta", None)
+            # cur_llm_name = "-- Not Selected --"
+            # if cur_llm != -1 and len(llm_configs) > cur_llm:
+            #     cur_llm_name = llm_configs[cur_llm]["alias"]
+            # gr.Text(cur_llm_name, label="Current AI")
 
-                    # list item: lesson entry card
-                    with gr.Column(variant="panel", elem_classes=["clickable-item", "lesson-item", "lesson-item-bg"], scale=0):
+            if cur_lesson == None:
+                with gr.Row():
+                    # list of lessons
+                    
+                    # TODO different sorting options
+                    # sort lessons by name
+                    sorted_lessons = sorted(lessons.items(), key=lambda item: item[1]["name"])
 
-                        # invisible button to trigger click event for the entire item card
-                        with gr.Row(scale=0, elem_classes=["lesson-name-wrapper"]):
-                            gr.Markdown(f"### {lesson_name}", elem_classes=["lesson-name"], scale=0, line_breaks=True)
-                            item_btn1 = gr.Button("", elem_classes=["lesson-click-button"])
-                            item_btn1.click(
+                    # render all the lesson items
+                    for lesson_name, lesson in sorted_lessons:
+                        # data
+                        meta = lesson.get("meta", None)
+
+                        # list item: lesson entry card
+                        with gr.Column(variant="panel", elem_classes=["clickable-item", "lesson-item", "lesson-item-bg"], scale=0):
+
+                            # invisible button to trigger click event for the entire item card
+                            with gr.Row(scale=0, elem_classes=["lesson-name-wrapper"]):
+                                gr.Markdown(f"### {lesson_name}", elem_classes=["lesson-name"], scale=0, line_breaks=True)
+                                item_btn1 = gr.Button("", elem_classes=["lesson-click-button"])
+                                item_btn1.click(
+                                    on_choose_lesson,
+                                    inputs=[gr.State(lesson_name), state_lessons],
+                                    outputs=[state_lessons, state_cur_lesson],
+                                )
+
+                            # TODO: render meta
+                            gr.Markdown(f"{meta['progress_idx']}", elem_classes=["lesson-meta"], scale=0, line_breaks=True)
+
+                            item_btn = gr.Button("", elem_classes=["lesson-click-button"])
+                            item_btn.click(
                                 on_choose_lesson,
                                 inputs=[gr.State(lesson_name), state_lessons],
                                 outputs=[state_lessons, state_cur_lesson],
                             )
+                            
+                    with gr.Column(variant="panel", elem_classes=["lesson-item"], scale=0):
+                        # the last item is the add button
+                        add_btn = gr.Button("➕", variant="secondary", elem_classes=["lesson-item"]) 
+                        add_btn.click(on_click_add_lesson, inputs=[], outputs=[])
 
-                        gr.Markdown(f"{meta['test']}", elem_classes=["lesson-meta"], scale=0, line_breaks=True)
+            else:
+                sheet = lessons[cur_lesson]["sheet"]
+                meta = lessons[cur_lesson]["meta"]
+                progress_idx = meta["progress_idx"]
 
-                        item_btn = gr.Button("", elem_classes=["lesson-click-button"])
-                        item_btn.click(
-                            on_choose_lesson,
-                            inputs=[gr.State(lesson_name), state_lessons],
-                            outputs=[state_lessons, state_cur_lesson],
-                        )
-                        
-                with gr.Column(variant="panel", elem_classes=["lesson-item"], scale=0):
-                    # the last item is the add button
-                    add_btn = gr.Button("➕", variant="secondary", elem_classes=["lesson-item"]) 
-                    add_btn.click(on_click_add_lesson, inputs=[], outputs=[])
-            
+                with gr.Row():
+                    exit_btn = gr.Button("↩", variant="secondary", size="sm", elem_classes=["exit-button"])
+                    exit_btn.click(on_exit_lesson, inputs=[], outputs=[state_cur_lesson])
+
+                    gr.Markdown(f"## Lesson: {cur_lesson}", elem_classes=["lesson-title"])
+
+                with gr.Column(elem_classes=["lesson-content"]):
+                    print(progress_idx)
+                    en = sheet[progress_idx, "EN"]
+                    cn = sheet[progress_idx, "CN"]
+                    id = sheet[progress_idx, "ID"]
+                    
+                    gr.Markdown(f"##  {cn}", elem_classes=["lesson-title"])
+                    gr.Markdown(f"##  {id}", elem_classes=["lesson-title"])
+                    gr.Markdown(f"##  {en}", elem_classes=["lesson-title"])
+                    
+
--- app/ui/ui_main.py
+++ app/ui/ui_main.py
@@ -5,11 +5,20 @@ from app.ui.tab_lesson import render_tab_lesson, load_lessons
 with gr.Blocks(fill_height=True) as demo: # 'demo' is a predefined name used for hot reloading
     # region UI Components
     state_lessons = gr.State(value={})
-    state_cur_lesson = gr.State(value=-1)
+    state_cur_lesson = gr.State(value=None)
     state_llm_configs = gr.State(value=[])
     state_cur_llm = gr.State(value=-1)
 
-    gr.Markdown("# Zeenom English")
+    with gr.Row():
+        with gr.Column(scale=1):
+            gr.Markdown("# Zeenom English", elem_classes=["main-title"], scale=1)
+        with gr.Column(scale=0, min_width=120):
+            @gr.render(inputs=[state_llm_configs, state_cur_llm])
+            def render_items(llm_configs: list[dict], cur_llm: int):   
+                cur_llm_name = "-- Not Selected --"
+                if cur_llm != -1 and len(llm_configs) > cur_llm:
+                    cur_llm_name = llm_configs[cur_llm]["alias"]
+                gr.Textbox(cur_llm_name, label="Current AI", scale=0)
     
     with gr.Row():
         render_tab_lesson(state_lessons, state_cur_lesson, state_llm_configs, state_cur_llm)
@@ -17,20 +26,20 @@ with gr.Blocks(fill_height=True) as demo: # 'demo' is a predefined name used for
         
 
     def on_load():
-        llm_configs, cur_llm = load_llm_configs()
         lessons, cur_lesson = load_lessons()
+        llm_configs, cur_llm = load_llm_configs()
         print("Loading Finished")
 
         return (
+            lessons,
+            cur_lesson,
             llm_configs, 
             cur_llm,
-            lessons,
-            cur_lesson
         )
 
     demo.load(on_load, outputs=[
-        state_llm_configs,
-        state_cur_llm,
         state_lessons,
         state_cur_lesson,
+        state_llm_configs,
+        state_cur_llm,
     ])
